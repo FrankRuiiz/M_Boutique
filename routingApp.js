22dcd55cd53c1ae5acae8b695cd40c7f5ba1e055
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-03-28 22:55:21 -0700

    Added responsive footer to main page with the required stylying - Frank

--- routingApp.js
+++ routingApp.js
@@ -4,33 +4,32 @@
 
 
 // Declaration of the Angular App
+var mboutiqueRoutApp = angular.module('mboutiqueRoutApp', ['ui.router']);
 
-var routerApp = angular.module('routerApp', ['ui.router']);
-
-routerApp.config(function($stateProvider, $urlRouterProvider) {
+mboutiqueRoutApp.config(function($stateProvider, $urlRouterProvider) {
+    //Default Route
     $urlRouterProvider.otherwise('/welcome');
 
+    //This is where routing is set for the nav bar
     $stateProvider
-
-        // Welcome state and nested views
+        // Welcome state
         .state('welcome', {
             url: '/welcome',
             templateUrl: 'templates/welcome_template.html'
         })
-        // Our Macarons
+        // Our Macarons state
         .state('our_macarons', {
             url: '/our_macarons',
             templateUrl: 'templates/our_macarons_template.html'
         })
-        // Gifts_Parties
+        // Gifts_Parties state
         .state('gifts_parties', {
             url: '/gifts_parties',
             templateUrl: 'templates/gifts_parties_template.html'
         })
-        // Contact Us
+        // Contact us state
         .state('contact', {
             url: '/contact',
             templateUrl: 'templates/contact_template.html'
         });
-
 });
