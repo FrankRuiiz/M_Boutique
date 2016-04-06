f30e10d90b8f56711a880367b13c9ab4cba03e94
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-04-06 16:02:22 -0700

    refactored mboutiqueApp.js and some of the name conventions - Frank

--- js/mboutiqueApp.js
+++ js/mboutiqueApp.js
@@ -2,63 +2,60 @@
  * Created by FrankyR on 3/28/2016.
  */
 
-
-// Declaration of the Angular App
+// Declared Angular App
 var mboutiqueApp = angular.module('mboutiqueApp', ['ui.router', 'pascalprecht.translate'])
 
-
-   .config(function ($provide) {
-        $provide.decorator('$uiViewScroll', function ($delegate) {
-            return function (uiViewElement) {
-                var top = uiViewElement.getBoundingClientRect().top;
-                window.scrollTo(0, (top - 30));
-            };
-        });
-    })
-
-    .config(function ($translateProvider) {
+    // App Configuration
+    .config(function ($translateProvider, $provide, $stateProvider, $urlRouterProvider) {
+        // Sets the default language to English
         $translateProvider.preferredLanguage('en');
 
+        // Static File Loader, used to grab language library from the languages directory
         $translateProvider.useStaticFilesLoader({
             prefix: 'languages/',
             suffix: '.json'
         });
+
+        //Sets the default rout to the welcom page
+        $urlRouterProvider.otherwise('/welcome');
+
+        //Rout settings for each link on the nav
+        $stateProvider
+        // Welcome state
+            .state('welcome', {
+                url: '/welcome',
+                templateUrl: 'templates/welcome_template.html'
+            })
+            // Our Macarons state
+            .state('our_macarons', {
+                url: '/our_macarons',
+                templateUrl: 'templates/our_macarons_template.html'
+            })
+            // Gifts_Parties state
+            .state('gifts_parties', {
+                url: '/gifts_parties',
+                templateUrl: 'templates/gifts_parties_template.html'
+            })
+            // Contact us state
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'templates/contact_template.html'
+            });
+
+        // Sets the view when clicking on links to the top of the window
+        $provide.decorator('$uiViewScroll', function () {
+            return function (uiViewElement) {};
+        });
     })
 
-
-    .controller('translateController', function($translate, $scope){
+    // mboutiqueApp controller
+    .controller('mboutiqueCtrl', function($translate, $scope){
         $scope.changeLanguage = function(langKey) {
             $translate.use(langKey);
         };
-    })
-
-
-    .config(function ($stateProvider, $urlRouterProvider) {
-    //Default Route
-    $urlRouterProvider.otherwise('/welcome');
-
-    //Routing is set here
-    $stateProvider
-    // Welcome state
-        .state('welcome', {
-            url: '/welcome',
-            templateUrl: 'templates/welcome_template.html'
-        })
-        // Our Macarons state
-        .state('our_macarons', {
-            url: '/our_macarons',
-            templateUrl: 'templates/our_macarons_template.html'
-        })
-        // Gifts_Parties state
-        .state('gifts_parties', {
-            url: '/gifts_parties',
-            templateUrl: 'templates/gifts_parties_template.html'
-        })
-        // Contact us state
-        .state('contact', {
-            url: '/contact',
-            templateUrl: 'templates/contact_template.html'
-        });
     });
+
+
+
 
 
