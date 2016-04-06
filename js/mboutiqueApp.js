33c3658efd9434bde0115d969e806ecb94e72108
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-04-06 15:41:09 -0700

    fixed issue where the page didn't link to the top of each page - Frank

---
+++ js/mboutiqueApp.js
@@ -0,0 +1,64 @@
+/**
+ * Created by FrankyR on 3/28/2016.
+ */
+
+
+// Declaration of the Angular App
+var mboutiqueApp = angular.module('mboutiqueApp', ['ui.router', 'pascalprecht.translate'])
+
+
+   .config(function ($provide) {
+        $provide.decorator('$uiViewScroll', function ($delegate) {
+            return function (uiViewElement) {
+                var top = uiViewElement.getBoundingClientRect().top;
+                window.scrollTo(0, (top - 30));
+            };
+        });
+    })
+
+    .config(function ($translateProvider) {
+        $translateProvider.preferredLanguage('en');
+
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'languages/',
+            suffix: '.json'
+        });
+    })
+
+
+    .controller('translateController', function($translate, $scope){
+        $scope.changeLanguage = function(langKey) {
+            $translate.use(langKey);
+        };
+    })
+
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+    //Default Route
+    $urlRouterProvider.otherwise('/welcome');
+
+    //Routing is set here
+    $stateProvider
+    // Welcome state
+        .state('welcome', {
+            url: '/welcome',
+            templateUrl: 'templates/welcome_template.html'
+        })
+        // Our Macarons state
+        .state('our_macarons', {
+            url: '/our_macarons',
+            templateUrl: 'templates/our_macarons_template.html'
+        })
+        // Gifts_Parties state
+        .state('gifts_parties', {
+            url: '/gifts_parties',
+            templateUrl: 'templates/gifts_parties_template.html'
+        })
+        // Contact us state
+        .state('contact', {
+            url: '/contact',
+            templateUrl: 'templates/contact_template.html'
+        });
+    });
+
+
